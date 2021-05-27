8b94437746bf1c8eb3bdc4b41aeff8242e3e39ab
D4ve-R <david.rechkemmer@gmail.com>
2021-05-27 10:33:13 +0200

    namespacing, git stuff

--- sorting.cpp
+++ sorting.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <array>
 
-#include "sorting.h"
+#include "Sorting.h"
 
-int main(){
-    std::vector<int> vec;
-    vec.push_back(8);
-    vec.push_back(3);
-    vec.push_back(6);
-    vec.push_back(2);
+using namespace AlgoLib::Sorting;
+
+int main()
+{
+    std::vector<int> vec = {8, 6, 2, 3};
     QuickSort<int>(vec, 0, 3);
     std::cout << vec[0] << vec[1] << vec[2] << vec[3] << std::endl;
 
-    int arr[] = {5, 6, 4, 3};
-    QuickSort<int>(arr, 0, 3);
-    std::cout << arr[0] << arr[1] << arr[2] << arr[3] << std::endl;
+    int c_Array[] = {5, 9, 4, 1};
+    QuickSort<int>(c_Array, 0, 3);
+    std::cout << c_Array[0] << c_Array[1] << c_Array[2] << c_Array[3] << std::endl;
     
+    std::array<int, 4> std_Array = {7, 2, 4, 1};
+    QuickSort<int>(std_Array, 0, 3);
+    std::cout << std_Array[0] << std_Array[1] << std_Array[2] << std_Array[3] << std::endl;
 }
